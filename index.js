38f1ee3603e35d79dd6db02af126cde99e89d2c2
lukechu1997 <lukechu421@gmail.com>
2020-01-22 17:35:59 +0800

    connect MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
+
+//models
+const TodoTask = require("./models/TodoTask");
+
+dotenv.config();
 
 app.use("/static", express.static("public"))
 
+app.use(express.urlencoded({ extended: true }));
+
+//connection to db
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true,  useUnifiedTopology: true }, () => {
+	console.log("Connected to db!");
+	// dedicating a port number and telling our express app to listen to that port.
+	app.listen(3000, () => console.log("Server Up and running"));
+});
+
 app.set("view engine", "ejs");
 
-app.listen(3000, () => console.log("Server Up and running"));
-// dedicating a port number and telling our express app to listen to that port.
-
+// GET method
 app.get('/',(req, res) => {
-    res.render('todo.ejs');
+	TodoTask.find({}, (err, tasks) => {
+		res.render("todo.ejs", { todoTasks: tasks });
+	});
+});
+
+// POST method
+app.post('/',async (req, res) => {
+	const todoTask = new TodoTask({
+		content: req.body.content
+	});
+	try {
+		await todoTask.save();
+		res.redirect("/");
+	} catch (err) {
+		res.redirect("/");
+	}
 });
