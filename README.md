f82a58249d2aaf053a0a99ab4cdbd14682763557
lukechu1997 <lukechu421@gmail.com>
2020-02-06 17:27:43 +0800

    Todo App finished

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # node_practice
+This project is made with node.js, ejs, Mongodb, Docker
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 //connection to db
 mongoose.set("useFindAndModify", false);
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true,  useUnifiedTopology: true }, () => {
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
 	console.log("Connected to db!");
 	// dedicating a port number and telling our express app to listen to that port.
 	app.listen(3000, () => console.log("Server Up and running"));
@@ -24,16 +24,14 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true,  useUnifiedTop
 app.set("view engine", "ejs");
 
 // GET method
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
 	TodoTask.find({}, (err, tasks) => {
-		console.log("rendering");
 		res.render("todo.ejs", { todoTasks: tasks });
 	});
-	// res.render('todo.ejs')
 });
 
 // POST method
-app.post('/',async (req, res) => {
+app.post('/', async (req, res) => {
 	const todoTask = new TodoTask({
 		content: req.body.content
 	});
@@ -43,5 +41,28 @@ app.post('/',async (req, res) => {
 	} catch (err) {
 		res.redirect("/");
 	}
-	console.log(req.body.content);
+});
+//UPDATE
+app
+	.route("/edit/:id")
+	.get((req, res) => {
+		const id = req.params.id;
+		TodoTask.find({}, (err, tasks) => {
+			res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+		});
+	})
+	.post((req, res) => {
+		const id = req.params.id;
+		TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+			if (err) return res.send(500, err);
+			res.redirect("/");
+		});
+	});
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+	const id = req.params.id;
+	TodoTask.findByIdAndRemove(id, err => {
+		if (err) return res.send(500, err);
+		res.redirect("/");
+	});
 });
